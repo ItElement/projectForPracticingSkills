7c1b49620bb36bab8ce3117357e31cc4adcabd7d
AlexandrS <solnsevalexandr@gmail.com>
2023-02-23 17:25:10 +0300

    add react testing library, add tests for btn, sidebar

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -3,17 +3,9 @@
  * https://jestjs.io/docs/configuration
  */
 
+import path from 'path';
+
 export default {
-    // All imported modules in your tests should be mocked automatically
-    // automock: false,
-
-    // Stop running tests after `n` failures
-    // bail: 0,
-
-    // The directory where Jest should store its cached dependency information
-    // cacheDirectory: "C:\\Users\\solns\\AppData\\Local\\Temp\\jest",
-
-    // Automatically clear mock calls, instances and results before every test
     clearMocks: true,
     testEnvironment: 'jsdom',
     coveragePathIgnorePatterns: [
@@ -34,10 +26,19 @@ export default {
     //     '**/__tests__/**/*.[jt]s?(x)',
     //     '**/?(*.)+(spec|test).[tj]s?(x)',
     // ],
+    modulePaths: [
+        '<rootDir>src',
+    ],
     testMatch: [
         '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
     ],
     rootDir: '../../',
+    setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
+    moduleNameMapper: {
+        '\\.s?css$': 'identity-obj-proxy',
+        // svg импортируется как готовый компонент поэтому делаем маппер
+        '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+    },
 
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
