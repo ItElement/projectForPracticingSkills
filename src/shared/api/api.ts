073c8d6a6568b6629452e7d56b3066a30cb3fab5
AlexandrS <solnsevalexandr@gmail.com>
2023-04-10 17:21:09 +0300

    fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -5,9 +5,17 @@ import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 export const $api = axios.create({
     baseURL: __API__,
     // для получение каких либо данных авторизованным пользователем
-    headers: {
-        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
-    },
+    // headers: {
+    //     authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
+    // },
+});
+
+// интерцептор отрабатывает перед любым запросом
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+    }
+    return config;
 });
 
 //
