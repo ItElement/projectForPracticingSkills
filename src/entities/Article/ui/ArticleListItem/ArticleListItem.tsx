0ecf4a49db0d7f93b369a6ff7b351dabbec17539
AlexandrS <solnsevalexandr@gmail.com>
2023-05-08 21:07:20 +0300

    refactoring router 92

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -7,7 +7,6 @@ import { Icon } from '@/shared/ui/Icon/Icon';
 import { Card } from '@/shared/ui/Card/Card';
 import { Avatar } from '@/shared/ui/Avatar/Avatar';
 import { Button, ButtonTheme } from '@/shared/ui/Button/Button';
-import { RoutePath } from '@/shared/config/routeConfig/routeConfig';
 import { AppLink } from '@/shared/ui/AppLink/AppLink';
 import { ArticleBlockType, ArticleView } from '../../model/consts/articleConsts';
 import {
@@ -17,6 +16,7 @@ import {
     Article, ArticleTextBlock,
 } from '../../model/types/article';
 import cls from './ArticleListItem.module.scss';
+import { RoutePath } from '@/shared/const/router';
 
 interface ArticleListItemProps {
     className?: string;
---
+++ src/shared/types/router.ts
@@ -0,0 +1,8 @@
+// расширяем дефолтные роуты
+import { RouteProps } from 'react-router-dom';
+import { UserRole } from '@/entities/User';
+
+export type AppRoutesProps = RouteProps & {
+    authOnly?: boolean;
+    roles?: UserRole[];
+}
